e8ab8c90ef2f000ed122e951878777c089991749
garfielder <lyqqing@126.com>
2018-05-30 22:37:11 +0800

    temp save

--- python/logisic/lg.py
+++ python/logisic/lg.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env  /tool/pandora64/hdk-4.8.1/19/bin/python2.7
+##!/usr/bin/env  /tool/pandora64/hdk-4.8.1/19/bin/python2.7
 
 ##
 # Linear regression
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 
@@ -31,14 +31,15 @@ def f(x, theta, a):
     return sum(x * theta + a)
 
 def GenSample():
-    num = 1000
+    num = 5000
     x1Start = 50.0
     x1End = 100.0
 
     x2Start = 0.0
     x2End = 30.0
 
-    theta = [1.25, -0.2]
+    theta = [1.25, -2.27]
+
 
     x1 = np.random.random(num) * (x1End - x1Start) + x1Start
     x2 = np.random.random(num) * (x2End - x2Start) + x2Start
@@ -47,29 +48,68 @@ def GenSample():
 
     # y = np.random.normal(0, 1, size= num)  + sum (x * theta )
     y = np.random.normal(0, 3, size= num)  + np.dot(theta, x)
-    z = [0 if val > 90.25 else 1 for val in y]
+    z = [0 if val > 60.25 else 1 for val in y]
 
     return x, z
 
 
 x, y  = GenSample()
 
+
+x = np.append(np.ones((1, x.shape[1])), x,  axis=0)
+
+
+
+def sigmoid(inX):
+    return 1.0/(1 + np.exp(-inX))
+
 ##
 # It is not easy to take a good alpha, two small,  convergence slow
 # too big cannot convergene
-alpha = 0.000000000001
+alpha = 0.001
 
 #plt.plot(x, y, "*")
 #plt.show()
 
-theta = [0.0, 0.0]
+theta = [1.0, 1.0, 1.0]
+
 for loop in range(0, 1000):
     deltaK = 0.0
     # loop each sample
-    hy = 1.0 / (1 + np.exp( np.dot( theta, x) ) )
+    # hy = 1.0 / (1 + np.exp( -np.dot( theta, x) ) )
+    hy = sigmoid(np.dot(theta, x))
 
-    theta = theta + alpha * np.sum( x * (y - hy), axis=1)
+    theta = theta + alpha /len(hy) * np.sum( x * (y - hy), axis=1)
 
     cost = 0.5 * (sum(y - hy) ** 2)
-    print ("Loop %d; K is [%f, %f];  cost is %f" % (loop, theta[0], theta[1],  cost ))
-    https://www.cnblogs.com/muchen/p/6296957.htmlhttps://www.cnblogs.com/muchen/p/6296957.html
+    if (loop % 100 == 0):
+        print ("Loop %d; K is [%f, %f, %f];  cost is %f" % (loop, theta[0], theta[1],  theta[2], cost ))
+
+
+x0 = []
+y0 = []
+x1 = []
+y1 = []
+for i in range(len(y)):
+    if (0 == y[i]):
+        x0.append( x[1][i] )
+        y0.append( x[2][i] )
+    else:
+        x1.append( x[1][i] )
+        y1.append( x[2][i] )
+            
+
+plt.scatter(x0, y0, c='red', marker = '*')
+plt.scatter(x1, y1, marker = 's')
+
+
+x = np.linspace(50.0,100.0, 50)
+y = (-theta[0] - theta[1] * x) / theta[2]
+
+plt.plot(x, y)
+
+
+
+plt.show()
+
+
