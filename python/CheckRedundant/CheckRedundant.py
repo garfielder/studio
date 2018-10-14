856c74b52f9020102fb3e84214bf080b630b32c4
garfielder <liyongqingcs@hotmail.com>
2018-10-14 21:17:19 +0800

    Update CheckRedundant.py

--- python/CheckRedundant/CheckRedundant.py
+++ python/CheckRedundant/CheckRedundant.py
@@ -6,14 +6,16 @@
 
 
 import os
+import filecmp
 
-rootdirs = [ "d:\\studio", 
-           ]
+rootdirs = [ "f:\lyq" ]
 
 resFile = "res.txt"
 
 fpRes = open(resFile, "w")
-
+if fpRes is None:
+    print ("Failed to open file " + resFile)
+print ("Differnt files are")
 files = {}
 for root in  rootdirs:
     for dirpath, dirnames, filenames in os.walk(root):
@@ -21,10 +23,24 @@ for root in  rootdirs:
             filename =  os.path.join(dirpath, filepath)
             basename =  os.path.basename(filename)
             if basename not in files.keys():
-                print("Adding %s " % filename)
+                #print("Adding %s " % filename)
                 files[basename] = filename
             else:
-                print("redundant> existing: %s  new is %s"  \
-                % (files[basename], filename), file = fpRes)
+                print >>fpRes, ("redundant> existing: %s  new is %s"  % (files[basename], filename))
 
-     
+
+###
+# Verify that no files are lost
+needGolden = True
+if (needGolden):
+    goldenName = "golden.txt"
+    fpGolden = open(goldenName, "w")
+    for file in files:
+        print >> fpGolden, file
+    fpGolden.close()
+
+###
+# compare with golden
+listName = "files.txt"
+fpList = open(listName, "w")
+for file in files:
